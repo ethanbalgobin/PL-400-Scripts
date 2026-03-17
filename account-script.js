3c12bcb2e58577a913aed43a1bb67da28b0d5198
Ethan Balgobin <ethanbalgo@hotmail.com>
2026-03-17 00:52:09 +0000

    fax field update

--- account-script.js
+++ account-script.js
@@ -1,7 +1,16 @@
 this.formOnLoad = function (executionContext) {
-  executionContext
-    .getFormContext()
-    .ui.setFormNotification("Hello world", "INFO", "IDUnique220912"); // message content, type of message, id of message
+  var formContext = executionContext.getFormContext();
+  formContext.ui.setFormNotification(
+    "Hello world v3",
+    "INFO",
+    "IDUnique220912",
+  ); // message content, type of message, id of message
 
-    
+  /**
+   * Get the column by the logical name, if it is blank update it, else leave the existing value.
+   * There are different types of contexts, examples, gridcontext, subgridcontext
+   */
+  if (formContext.getAttribute("fax").getValue() == null) {
+    formContext.getAttribute("fax").setValue("123-4567");
+  }
 };
