1118cf80090bdcbf6bdb28b228b345bd544a9ed8
Ethan Balgobin <ethanbalgo@hotmail.com>
2026-03-17 00:34:04 +0000

    on load message

---
+++ account-script.js
@@ -0,0 +1,7 @@
+this.formOnLoad = function (executionContext) {
+  executionContext
+    .getFormContext()
+    .ui.setFormNotification("Hello world", "INFO", "IDUnique220912"); // message content, type of message, id of message
+
+    
+};
